cff3b8662db4bdc56bf1963e3a7f217d1bea0b19
Elijah Perrault <elijah@starcoder.info>
2011-01-19 14:42:53 -0700

    Begin on the IRC parser.

---
+++ src/Parser/IRC.pm
@@ -0,0 +1,10 @@
+# Auto IRC Bot. An advanced, lightweight and powerful IRC bot.
+# Copyright (C) 2010-2011 Xelhua Development Team (doc/CREDITS)
+# This program is free software; rights to this code are stated in doc/LICENSE.
+
+# Subroutines for parsing incoming data from IRC.
+package Parser::IRC;
+use strict;
+use warnings;
+
+
