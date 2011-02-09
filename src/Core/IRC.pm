b97d6e45ab02a92fd862020a556892068bae3124
Elijah Perrault <elijah@starcoder.info>
2011-02-09 14:45:53 -0700

    Finished VERSION reply.

--- src/Core/IRC.pm
+++ src/Core/IRC.pm
@@ -6,12 +6,13 @@
 package Core::IRC;
 use strict;
 use warnings;
+use English qw(-no_match_vars);
 use API::Std qw(hook_add hook_del);
+use API::IRC qw(notice usrc);
 our $VERSION = 3.000000;
 
 # CTCP VERSION reply.
-sub version_reply
-{
+hook_add("on_uprivmsg", "ctcp_version_reply", sub {
     my (($svr, @ex)) = @_;
     my %src = usrc(substr($ex[0], 1));
 
@@ -25,4 +26,4 @@ sub version_reply
     }
 
     return 1;
-}
+});
