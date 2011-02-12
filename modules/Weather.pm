a8134f6270d18b9e56f2155f6b376a412b50bd39
Elijah Perrault <elijah@starcoder.info>
2011-02-11 20:14:49 -0700

    Fix the copyright headers. Just noticed this.

--- modules/Weather.pm
+++ modules/Weather.pm
@@ -1,5 +1,5 @@
 # Auto IRC Bot. An advanced, lightweight and powerful IRC bot.
-# Copyright (C) 2010-2011 Xelhua Development Team (doc/CREDITS)
+# Copyright (C) 2010-2011 Xelhua Development Group, et al.
 # This program is free software; rights to this code are stated in doc/LICENSE.
 package m_Weather;
 use strict;
